f9011aeaaf546d13c08e57aaf9b32a510253882d
Raymond Deng <1107450@gmail.com>
2023-08-24 04:23:50 -0700

    add some init files for buffer pool manager

---
+++ src/buffer/lru_replacer.cpp
@@ -0,0 +1,17 @@
+#include "buffer/lru_replacer.h"
+
+namespace dbfs {
+
+LRUReplacer::LRUReplacer(size_t num_pages) {}
+
+LRUReplacer::~LRUReplacer() = default;
+
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+
+void LRUReplacer::Pin(frame_id_t frame_id) {}
+
+void LRUReplacer::Unpin(frame_id_t frame_id) {}
+
+auto LRUReplacer::Size() -> size_t { return 0; }
+
+}  // namespace dbfs
